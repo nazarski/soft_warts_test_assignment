d915727e871227447aed8f40ec41428beef093d0
nazarski <110051407+nazarski@users.noreply.github.com>
2023-05-22 12:54:18 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+
+
+  runApp(const ToDoShechka());
+}
+
+
+class ToDoShechka extends StatelessWidget {
+  const ToDoShechka({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+
+///Colors will be used in app UI
+class AppColors{
+
+  static const Color primary = Color(0xffDBDBDB);
+  static const Color primaryVariant = Color(0xffFFD600);
+
+  static const Color secondary = Color(0xffA9A9A9);
+  static const Color secondaryVariant = Color(0xff383838);
+
+  static const Color accentRed = Color(0xffFF8989);
+  static const Color disabled = Color(0xffFBEFB4);
+
+}
